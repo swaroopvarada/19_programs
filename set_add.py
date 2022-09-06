77fde643c6bd8c3bc4adf0b76b46123fe47e779f
swaroopvarada <110213117+swaroopvarada@users.noreply.github.com>
2022-09-06 21:40:28 +0530

    Add files via upload

---
+++ set_add.py
@@ -0,0 +1,8 @@
+"""hackerrank-set.add() """
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+NAMES = set([])
+for i in range(n):
+    NAMES.add(input())
+print(len(NAMES))
